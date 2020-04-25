06e2f937136017574731623c17c82900dd9da05b
Ben <parant.b@gmail.com>
2020-04-25 11:49:09 +0200

    Added favicons and finished form validation

--- src/components/SplashScreenForm/SplashScreenForm.js
+++ src/components/SplashScreenForm/SplashScreenForm.js
@@ -12,112 +12,154 @@ import { hideSplashScreen } from 'redux/actions/SplashScreenActions';
 import './SplashScreenForm.scss';
 
 function SplashScreenForm() {
+  const dispatch = useDispatch();
+
+  // Name Field state declaration, change event handler and validator
   const [nameValue, setNameValue] = useState('');
   const [nameErrorValue, setNameErrorValue] = useState(false);
   const handleNameChange = (event) => {
     setNameValue(event.target.value);
-    if (event.target.value.length > 0 && event.target.value.length < 40) {
+    validateNameValue(event.target.value);
+  };
+  const validateNameValue = (value) => {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 40) {
       setNameErrorValue(false);
+      returnValue = true;
     } else {
       setNameErrorValue(true);
     }
+    return returnValue;
   };
 
+  // Surname Field state declaration, change event handler and validator
   const [surnameValue, setSurnameValue] = useState('');
   const [surnameErrorValue, setSurnameErrorValue] = useState(false);
   const handleSurnameChange = (event) => {
     setSurnameValue(event.target.value);
-    if (event.target.value.length > 0 && event.target.value.length < 40) {
+    validateSurnameValue(event.target.value);
+  };
+  const validateSurnameValue = (value) => {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 40) {
       setSurnameErrorValue(false);
+      returnValue = true;
     } else {
       setSurnameErrorValue(true);
     }
+    return returnValue;
   };
 
+  // Birthday Field state declaration, change event handler and validator
   const [birthdayValue, setBirthdayValue] = useState('');
   const [birthdayErrorValue, setBirthdayErrorValue] = useState(false);
   const handleBirthdayChange = (event) => {
+    setBirthdayValue(event.target.value);
+    validateBirthdayValue(event.target.value);
+  };
+  const validateBirthdayValue = (value) => {
     const birthdayRegex = RegExp(
       '^(19|20)\\d\\d([- /.])(0[1-9]|1[012])\\2(0[1-9]|[12][0-9]|3[01])$',
     );
-    setBirthdayValue(event.target.value);
-    if (
-      event.target.value.length > 0 &&
-      event.target.value.match(birthdayRegex)
-    ) {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 11 && value.match(birthdayRegex)) {
       setBirthdayErrorValue(false);
+      returnValue = true;
     } else {
       setBirthdayErrorValue(true);
     }
+    return returnValue;
   };
 
+  // Birth Place Field state declaration, change event handler and validator
   const [birthPlaceValue, setBirthPlaceValue] = useState('');
   const [birthPlaceErrorValue, setBirthPlaceErrorValue] = useState(false);
   const handleBirthPlaceChange = (event) => {
     setBirthPlaceValue(event.target.value);
-    if (event.target.value.length > 0 && event.target.value.length < 40) {
+    validateBirthPlaceValue(event.target.value);
+  };
+  const validateBirthPlaceValue = (value) => {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 30) {
       setBirthPlaceErrorValue(false);
+      returnValue = true;
     } else {
       setBirthPlaceErrorValue(true);
     }
+    return returnValue;
   };
 
+  // Address Field state declaration, change event handler and validator
   const [addressValue, setAddressValue] = useState('');
   const [addressErrorValue, setAddressErrorValue] = useState(false);
   const handleAddressChange = (event) => {
     setAddressValue(event.target.value);
-    if (event.target.value.length > 0 && event.target.value.length < 60) {
+    validateAddressValue(event.target.value);
+  };
+  const validateAddressValue = (value) => {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 60) {
       setAddressErrorValue(false);
+      returnValue = true;
     } else {
       setAddressErrorValue(true);
     }
+    return returnValue;
   };
 
+  // City Field state declaration, change event handler and validator
   const [cityValue, setCityValue] = useState('');
   const [cityErrorValue, setCityErrorValue] = useState(false);
   const handleCityChange = (event) => {
     setCityValue(event.target.value);
-    if (event.target.value.length > 0 && event.target.value.length < 30) {
+    validateCityValue(event.target.value);
+  };
+  const validateCityValue = (value) => {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 30) {
       setCityErrorValue(false);
+      returnValue = true;
     } else {
       setCityErrorValue(true);
     }
+    return returnValue;
   };
 
+  // Postal Code Field state declaration, change event handler and validator
   const [postalCodeValue, setPostalCodeValue] = useState('');
   const [postalCodeErrorValue, setPostalCodeErrorValue] = useState(false);
+  const postalCodeRegex = RegExp('\\b\\d{5}\\b');
   const handlePostalCodeChange = (event) => {
     setPostalCodeValue(event.target.value);
-    if (event.target.value.length > 0 && event.target.value.length < 6) {
+    validatePostalCodeValue(event.target.value);
+  };
+  const validatePostalCodeValue = (value) => {
+    let returnValue = false;
+    if (value.length > 0 && value.length < 30 && value.match(postalCodeRegex)) {
       setPostalCodeErrorValue(false);
+      returnValue = true;
     } else {
       setPostalCodeErrorValue(true);
     }
+    return returnValue;
   };
 
-  const dispatch = useDispatch();
-
+  // Form Validator
   const formIsCorrect = () => {
     return (
-      !nameErrorValue &&
-      nameValue &&
-      !surnameErrorValue &&
-      surnameValue &&
-      !birthdayErrorValue &&
-      birthdayValue &&
-      !birthPlaceErrorValue &&
-      birthPlaceValue &&
-      !addressErrorValue &&
-      addressValue &&
-      !cityErrorValue &&
-      cityValue &&
-      !postalCodeErrorValue &&
-      postalCodeValue
+      validateNameValue(nameValue) &&
+      validateSurnameValue(surnameValue) &&
+      validateBirthdayValue(birthdayValue) &&
+      validateBirthPlaceValue(birthPlaceValue) &&
+      validateAddressValue(addressValue) &&
+      validateCityValue(cityValue) &&
+      validatePostalCodeValue(postalCodeValue)
     );
   };
 
+  // Form submission handler
   const handleFormSubmit = () => {
-    if (!formIsCorrect) {
+    if (formIsCorrect()) {
       const values = {
         name: nameValue,
         surname: surnameValue,
@@ -137,7 +179,10 @@ function SplashScreenForm() {
       <Container>
         <Row className="justify-content-center">
           <Col xs={10} md={6}>
-            <form id="splash-screen-form">
+            <form
+              id="splash-screen-form"
+              onSubmit={handleFormSubmit.bind(this)}
+            >
               <TextField
                 className="splash-screen-form-input"
                 label="Nom"
@@ -145,7 +190,7 @@ function SplashScreenForm() {
                 onChange={handleNameChange.bind(this)}
                 error={nameErrorValue}
                 inputProps={{
-                  autocomplete: 'family-name',
+                  autoComplete: 'family-name',
                 }}
               />
               <TextField
@@ -155,7 +200,7 @@ function SplashScreenForm() {
                 onChange={handleSurnameChange.bind(this)}
                 error={surnameErrorValue}
                 inputProps={{
-                  autocomplete: 'given-name',
+                  autoComplete: 'given-name',
                 }}
               />
               <TextField
@@ -169,7 +214,7 @@ function SplashScreenForm() {
                   shrink: true,
                 }}
                 inputProps={{
-                  autocomplete: 'bday',
+                  autoComplete: 'bday',
                 }}
               />
               <TextField
@@ -186,7 +231,7 @@ function SplashScreenForm() {
                 onChange={handleAddressChange.bind(this)}
                 error={addressErrorValue}
                 inputProps={{
-                  autocomplete: 'address-line1',
+                  autoComplete: 'address-line1',
                 }}
               />
               <TextField
@@ -196,7 +241,7 @@ function SplashScreenForm() {
                 onChange={handleCityChange.bind(this)}
                 error={cityErrorValue}
                 inputProps={{
-                  autocomplete: 'address-level2',
+                  autoComplete: 'address-level2',
                 }}
               />
               <TextField
@@ -206,8 +251,8 @@ function SplashScreenForm() {
                 onChange={handlePostalCodeChange.bind(this)}
                 error={postalCodeErrorValue}
                 inputProps={{
-                  autocomplete: 'postal-code',
-                  inputmode: 'numeric',
+                  autoComplete: 'postal-code',
+                  inputMode: 'numeric',
                   pattern: '[0-9]{5}',
                   min: '00000',
                   max: '99999',
@@ -217,8 +262,7 @@ function SplashScreenForm() {
                 id="get-started-button"
                 variant="contained"
                 color="primary"
-                type="submit"
-                onClick={handleFormSubmit}
+                onClick={handleFormSubmit.bind(this)}
               >
                 Valider <ArrowForwardIcon />
               </Button>
